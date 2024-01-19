401f90bb3715d2d0b2bda070ac4edbd554778a6d
Sagar Raina <rainasagar7@gmail.com>
2024-01-20 01:58:34 +0530

    ok

--- leetCode/practice.py
+++ leetCode/practice.py
@@ -1,10 +1,4 @@
-def isPrime(number):
-    isprime = True
-    for i in range(2, number):
-        if number%i == 0:
-            isprime = False
-    if isprime:
-        print(f"{number} is a prime number")
-    else:
-        print(f"{number} is not a prime number")
-isPrime(12)
+a = '[]{}()'
+for i in a:
+    print(i)
+print(a[2])
